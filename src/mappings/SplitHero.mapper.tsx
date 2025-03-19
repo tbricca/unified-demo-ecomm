c42ee7487efe89bff4b4697be8b044b85de6474c
Nick Nestle <nick.nestle@gmail.com>
2025-03-19 07:02:01 -0700

    With Split Hero Mapped

---
+++ src/mappings/SplitHero.mapper.tsx
@@ -0,0 +1,33 @@
+import { figmaMapping, type BaseFigmaProps } from "@builder.io/dev-tools/figma";
+import SplitHero from "@/components/Hero/SplitHero";
+
+// ❖ SplitHero
+interface FigmaSplitHeroProps extends BaseFigmaProps {
+  ButtonText?: string;
+  Subtitle?: string;
+  imageUrl?: string;
+  Title?: string;
+}
+
+// Read more at https://www.builder.io/c/docs/mapping-functions
+figmaMapping({
+  componentKey: "06a3a922620c3c62e1365c646dab3eed590c1e46",
+  mapper(figma: FigmaSplitHeroProps) {
+    return (
+      <SplitHero
+        imageAlignment="right"
+        textAlignment="left"
+        splitWidth="1/2"
+        title={figma.Title ?? ""}
+        subTitle={figma.Subtitle ?? ""}
+        image={figma.imageUrl ?? ""}
+        altText="Hero Image"
+        hasCTA={!!figma.ButtonText}
+        buttonText={figma.ButtonText}
+        buttonLink="#"
+        makeFullBleed={false}
+        shrinkImage="shrink"
+      />
+    );
+  },
+});
